488d3581c187cc3a6151232202d484575f7fdec5
Eric Green <eric.green@spirent.com>
2016-06-03 17:25:42 -0700

    Rename cloudstress agent consumer plugin to host agent consumer plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -2,5 +2,5 @@ package all
 
 import (
 	_ "github.com/SpirentOrion/metrics-service/telegraf/plugins/inputs/cloudstress_agent_consumer"
-	_ "github.com/SpirentOrion/metrics-service/telegraf/plugins/inputs/cloudstress_host_consumer"
+	_ "github.com/SpirentOrion/metrics-service/telegraf/plugins/inputs/host_agent_consumer"
 )
