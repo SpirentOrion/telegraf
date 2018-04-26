42385d30fc8f9536f38ed59647d47ec5d0c009e5
Richard Myers <Richard.Myers@spirent.com>
2018-04-26 13:29:53 -1000

    Sync protect instance map and reduce glimpse calls (#12)
    * Sync protect instance map and reduce glimpse calls
    
    * Code review cleanup

--- plugins/inputs/host_agent_consumer/host_agent_consumer_test.go
+++ plugins/inputs/host_agent_consumer/host_agent_consumer_test.go
@@ -1,6 +1,7 @@
 package host_agent_consumer
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -34,4 +35,75 @@ func TestGlimpseArgs(t *testing.T) {
 		"-addr", "1.0.0.1",
 		"list", "hypervisors",
 	})
+}
+
+func BenchmarkMacAddrNetworkMapNoLock(b *testing.B) {
+	portMapRef := createNetPortMap()
+	for i := 0; i < b.N; i++ {
+		portMap := portMapRef
+		p, ok := portMap["0:0:0:0:0:1"]
+		if !ok {
+			b.Fatalf("portMap find failed")
+		}
+		if p.MacAddress != "0:0:0:0:0:1" {
+			b.Fatalf("portMap port failed")
+		}
+	}
+}
+
+func BenchmarkMacAddrNetworkMap(b *testing.B) {
+	h := &HostAgent{}
+	portMap := createNetPortMap()
+	h.cloudMacAddrNetworkMap.Store(portMap)
+	for i := 0; i < b.N; i++ {
+		portMap := h.cloudMacAddrNetworkMapLoad()
+		p, ok := portMap["0:0:0:0:0:1"]
+		if !ok {
+			b.Fatalf("portMap find failed")
+		}
+		if p.MacAddress != "0:0:0:0:0:1" {
+			b.Fatalf("portMap port check failed")
+		}
+	}
+}
+
+func BenchmarkCloudInstances(b *testing.B) {
+	h := &HostAgent{
+		cloudInstances: make(map[string]*CloudInstance),
+	}
+	for i := 4; i < 6; i++ {
+		c := &CloudInstance{
+			Name: fmt.Sprintf("instance-%d", i),
+			Id:   fmt.Sprintf("%d", i),
+		}
+		h.setCloudInstance(c.Id, c)
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		c, ok := h.cloudInstance("5")
+		if !ok {
+			b.Fatalf("cloud instance find failed")
+		}
+		if c.Id != "5" {
+			b.Fatalf("cloud instance check failed")
+		}
+	}
+}
+
+func createNetPortMap() CloudMacAddrNetworkMap {
+	portMap := make(CloudMacAddrNetworkMap)
+	ports := []CloudNetworkPort{
+		CloudNetworkPort{
+			MacAddress:  "0:0:0:0:0:1",
+			NetworkName: "br-eth0",
+		},
+		CloudNetworkPort{
+			MacAddress:  "0:0:0:0:0:2",
+			NetworkName: "br-eth1",
+		},
+	}
+	for p := range ports {
+		portMap[ports[p].MacAddress] = &ports[p]
+	}
+	return portMap
 }
